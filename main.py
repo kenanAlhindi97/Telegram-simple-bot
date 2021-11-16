2e2ab57717025b800df7d88b65d7baaad6db6821
kenan.alhindi97@outlook.com <kenan.alhindi97@outlook.com>
2021-11-16 18:19:06 +0100

    clean up

--- main.py
+++ main.py
@@ -1,19 +1,4 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-from telegram.ext import Updater
-from telegram.ext import Dispatcher
-from telegram.ext import CommandHandler
-import logging
 import telebot
-
-
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="عيب يا شب أنا البوت شيخ الشباب")
-
 
 bot_token = "2032034697:AAFLQH09fuTXAFCjr7uVa6SUXFAqGhyUc08"
 bot = telebot.TeleBot(bot_token, parse_mode=None)
